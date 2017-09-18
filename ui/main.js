dd994a3d9a44db128fe604b776be5b128022e4bd
Pavithracsekar <30979802+Pavithracsekar@users.noreply.github.com>
2017-09-18 22:10:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var element = document.getElementById('main-text')
 };
 element.innerHTML='New Value'; 
 
-var img= document.getElementId('img');
+var img= document.getElementId('mardi');
 img.onclick=function(){
     img.style.marginleft='100px';
 };
