9e7ec75c4f1a0ae1acba7efad66915b39710ca9e
Pavithracsekar <30979802+Pavithracsekar@users.noreply.github.com>
2017-09-18 19:14:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ app.get('/', function (req, res) {
 app.get('/article-one',function(req,res){
      res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
-app.get('/article-two',function(req,res){
-    res.send('Article two requested will be served here');
+app.get('/article-2',function(req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'article-2.html'));
 });
 app.get('/article-three',function(req,res){
-    res.send('Article three requested will be served here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
